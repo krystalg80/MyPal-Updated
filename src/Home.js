574af3d9ca0a12f1209027804013c78b275cb12b
krystalg80 <krystalsoccer80@gmail.com>
2025-02-23 13:07:25 -0800

    finished home

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
     import React from 'react';
+    import { Link } from 'react-router-dom';
     import video from './assets/dogs.mov';
     import './Home.css';
     import doggie from './assets/doggie.png'
@@ -31,11 +32,33 @@
             <div className="company-info">
                     <div className="company-info-content">
                         <h1>Never Lose Your Best Friend Again</h1>
-                        <h2>Peace of mind with advanced GPS tracking for your pets</h2>
-                        <img src={doggie} alt="Dog" width="350px" />
+                        <h3>Peace of mind with advanced GPS tracking for your pets</h3>
+                        <Link to="/buynow" className="home-btn">Shop Now</Link>
+                        <img src={doggie} alt="Dog" width="275px" />
                         {/* <h2>Did you know 10 million dogs are lost a year? From those missing dogs more than 80% are never found. Heartbreaking. MyPal was created to provide pet owners an affordable GPS Dog Collar that allows you to know your dog's whereabouts wherever and whenever! Help us prevent the grief of our lost pets!</h2> */}
                     </div>
                 </div>      
+            </div>
+        </div>
+        <div className="col-md-10 mx-auto">
+            <div className="about-container">
+                <div className="about-info">
+                    <h1>Why MyPal Matters</h1>
+                    <div className="facts-container">
+                        <div className="fact-item">
+                            <div className="fact-number">10M</div>
+                            <p>Pets are lost each year in the United States</p>
+                        </div>
+                        <div className="fact-item">
+                            <div className="fact-number">80%</div>
+                            <p>Of lost pets are never reunited with their owners</p>
+                        </div>
+                        <div className="fact-item">
+                            <div className="fact-number">100%</div>
+                            <p>Peace of mind with MyPal GPS tracking</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         {/* <div className="col-md-10 mx-auto">
